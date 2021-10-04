9b4024f6e8b1662c2c64dcb5c1617c2a5d5bd8d7
AlexToW <trishin.as@phystech.edu>
2021-10-04 11:15:27 +0300

    increasing offset

--- 03.2_copy.c
+++ 03.2_copy.c
@@ -8,19 +8,19 @@
 #include <string.h>
 
 
-#define BUFFSIZE        1024
+#define BUFFSIZE    1024
 
 
-int pwrite_all(int out_fd, const void* buf, ssize_t bytes_r, off_t out_offset) {
+int pwrite_all(int out_fd, const void* buf, ssize_t bytes_r, off_t* out_offset) {
     ssize_t bytes_w;
     while(bytes_r) {
-        bytes_w = pwrite(out_fd, buf, bytes_r, out_offset);
+        bytes_w = pwrite(out_fd, buf, bytes_r, *out_offset);
         if(bytes_w == -1) {
             perror("Bad writing");
             return 1;
         }
         bytes_r -= bytes_w;
-        out_offset += bytes_w;
+        *out_offset += bytes_w;
     }
     return 0;
 }
@@ -29,30 +29,29 @@ int pwrite_all(int out_fd, const void* buf, ssize_t bytes_r, off_t out_offset) {
 int main(int argc, char* argv[]) {
     if(argc != 3) {
         fprintf(stderr, "Usage: %s path text\n", argv[0]);
-        return EXIT_FAILURE;
+        return 1;
     }
     struct stat sb;
     if(lstat(argv[1], &sb) == -1) {
         perror("lstat");
-        exit(EXIT_FAILURE);
+        return 2;
     }
     if((sb.st_mode & S_IFMT) != S_IFREG) {
         fprintf(stderr, "Not a regular file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
+        return 3;
     }
 
 
     int in_fd = open(argv[1], O_RDONLY);
     if (in_fd == -1) {
         perror("Cannot open file to read");
-        return errno;
+        return 4;
     }
 
     int out_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); // хотим и писать, и читать
     if (out_fd == -1) {
-        int err = errno;
         close(in_fd);
-        return err;
+        return 5;
     }
     off_t in_offset = 0, out_offset = 0;
     char buf[BUFFSIZE];
@@ -69,17 +68,17 @@ int main(int argc, char* argv[]) {
         in_offset += bytes_r;
 
         // непосредственно запись
-        if(pwrite_all(out_fd, buf, bytes_r, out_offset) != 0) {
-            return 1;
+        if(pwrite_all(out_fd, buf, bytes_r, &out_offset) != 0) {
+            return 6;
         }
     }
     if(close(in_fd)) {
         perror("Bad closing");
-        return 3;
+        return 7;
     }
     if(close(out_fd)) {
         perror("Bad closing");
-        return 4;
+        return 8;
     }
     return 0;
 }
