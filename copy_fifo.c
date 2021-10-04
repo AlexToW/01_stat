00404050d846bc26cfd4ef1a6e76212c958ac42e
AlexToW <trishin.as@phystech.edu>
2021-10-04 12:11:50 +0300

    fifo

--- copy_fifo.c
+++ copy_fifo.c
@@ -11,43 +11,29 @@
 int main(int argc, char* argv[]) {
     if(argc != 3) {
         fprintf(stderr, "Bad argc: %d\n", argc);
-        exit(EXIT_FAILURE);
+        return 1;
     }
     struct stat sb_src, sb_dst;
     if(lstat(argv[1], &sb_src) == -1) {
         perror("lstat");
-        exit(EXIT_FAILURE);
+        return 2;
     }
     // работаем только с FIFO/pipe
-    if(sb_src.st_mode && S_IFMT != S_IFIFO) {
-        perror("Not a FIFO/pipe file");
-        exit(EXIT_FAILURE);
-    }
-	/*
-		
-	*/
-	// права источника
-	struct stat sb;
-	if(lstat(argv[1], &sb) == -1) {
-		perror("lstat");
-		exit(EXIT_FAILURE);
-	}
-    /* 
-        errno = ENOENT, если компонент пути pathname не существует 
-        или в pathname указана пустая строка. Случай pathname = ""
-        обработаем отдельно.
-    */
-    if(lstat(argv[2], &sb_dst) == -1) {
-        if(errno == ENOENT && strlen(argv[2]) == 0) {
-            // destination_file не существует
-            if(mkfifo(argv[2], sb.st_mode) == -1) { // 0777 = r+w+x for all users
-                fprintf(stderr, "Failure to create FIFO file at %s\n", argv[2]);
-                exit(EXIT_FAILURE);
+    if((sb_src.st_mode & S_IFMT) == S_IFIFO) {
+        /* 
+            errno = ENOENT, если компонент пути pathname не существует 
+            или в pathname указана пустая строка. Случай pathname = ""
+            обработаем отдельно.
+        */
+        if(lstat(argv[2], &sb_dst) == -1) {     // хотим обработать именно случай ошибки
+            if(errno == ENOENT && strlen(argv[2]) != 0) {
+                // destination_file doesn't exist
+                if(mkfifo(argv[2], sb_src.st_mode) == -1) {
+                    fprintf(stderr, "Failure to create FIFO %s\n", argv[2]);
+                    return 3;
+                }
             }
-        } else {
-            perror("lstat dst_file");
-            exit(EXIT_FAILURE);
         }
     }
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
