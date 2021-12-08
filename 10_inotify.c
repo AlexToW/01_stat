ea540894a4d53cbbdc1b6ae698930b9720d407d8
AlexToW <trishin.as@phystech.edu>
2021-12-08 12:38:56 +0300

    task 10

--- 10_inotify.c
+++ 10_inotify.c
@@ -1,3 +1,21 @@
+#if 0
+/* Supported events suitable for MASK parameter of INOTIFY_ADD_WATCH.  */
+#define IN_ACCESS	 0x00000001	/* File was accessed.  */
+#define IN_MODIFY	 0x00000002	/* File was modified.  */
+#define IN_ATTRIB	 0x00000004	/* Metadata changed.  */
+#define IN_CLOSE_WRITE	 0x00000008	/* Writtable file was closed.  */
+#define IN_CLOSE_NOWRITE 0x00000010	/* Unwrittable file closed.  */
+#define IN_CLOSE	 (IN_CLOSE_WRITE | IN_CLOSE_NOWRITE) /* Close.  */
+#define IN_OPEN		 0x00000020	/* File was opened.  */
+#define IN_MOVED_FROM	 0x00000040	/* File was moved from X.  */
+#define IN_MOVED_TO      0x00000080	/* File was moved to Y.  */
+#define IN_MOVE		 (IN_MOVED_FROM | IN_MOVED_TO) /* Moves.  */
+#define IN_CREATE	 0x00000100	/* Subfile was created.  */
+#define IN_DELETE	 0x00000200	/* Subfile was deleted.  */
+#define IN_DELETE_SELF	 0x00000400	/* Self was deleted.  */
+#define IN_MOVE_SELF	 0x00000800	/* Self was moved.  */
+#endif
+
 /*
     Появление новых файлов в указанном каталоге
 */
@@ -8,7 +26,10 @@
 #include <sys/inotify.h>
 #include <unistd.h>
 
+
 #define SIZEOF_BUF  4096
+#define DEBUG
+
 
 void handle_event(int fd, int wd, char* pathname) {
     char buf[SIZEOF_BUF];
@@ -27,21 +48,19 @@ void handle_event(int fd, int wd, char* pathname) {
 
         for(ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
             event = (const struct inotify_event*)buf;
-            if(event->mask & IN_CREATE) {
-                printf("IN_CREATE: ");
+            if(event->cookie != 0 && (event->mask & IN_MOVE)) {
+                // IN_MOVED_FROM & IN_MOVED_TO
+                if(event->len && wd == event->wd)
+                    printf("IN_MOVE: %s/%s", pathname, event->name);
+            } else if(event->mask & IN_CREATE) {
+                if(event->len && wd == event->wd)
+                    printf("IN_CREATE: %s/%s", pathname, event->name);
             }
-        }
-        if(wd == event->wd) {
-            printf("%s/", pathname);
-        }
-        if(event->len) {
-            printf("%s", event->name);
-        }
-
-        if(event->mask & IN_ISDIR) {
-            printf(" [directory]\n");
-        } else {
-            printf(" [file]\n");
+            if(event->mask & IN_ISDIR) {
+                printf("\t(directory)\n");
+            } else {
+                printf("\t(file)\n");
+            }
         }
     }
 }
@@ -58,13 +77,15 @@ int main(int argc, char* argv[]) {
     nfds_t nfds;
     struct pollfd fds[2];
 
+    // создаем экземпляр inotify
     fd = inotify_init1(IN_NONBLOCK);
     if(fd == -1) {
         perror("inotify_init1");
         exit(EXIT_FAILURE);
     }
 
-    wd = inotify_add_watch(fd, argv[1], IN_CREATE);
+    // наблюдаем за argv[1] на предмет создания новых файлов(IN_CREATE)
+    wd = inotify_add_watch(fd, argv[1], IN_MOVE | IN_CREATE);
     if(wd == -1) {
         perror("inotify_add_watch");
         exit(EXIT_FAILURE);
