4c79c6df2d86be44ed9f5fd7860ddf03b085e969
AlexToW <trishin.as@phystech.edu>
2021-12-19 00:39:06 +0300

    groups

--- 12.c
+++ 12.c
@@ -15,6 +15,7 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <limits.h>
 
 
 #define NGROUPS 20
@@ -22,6 +23,56 @@
 
 
 int main(void) {
+    /*
+        1. Process ID (getpid(2))
+        2. Parent process ID (getppid(2))
+        3. Process group ID and session ID
+        4. User and group identifiers.  A process can obtain its session ID using  getsid(2), 
+        and its process group ID using getpgrp(2).
+            4.1  Real  user ID and real group ID (getgid(2))
+            4.2 Effective user ID and effective group ID (getegid(2))
+    */
+
+    // pid, ppid
+    pid_t pid = getpid();
+    pid_t ppid = getppid();
+    printf("PID: %d,\t\t PPID: %d\n", pid, ppid);
+    // Process group ID and session ID
+    pid_t session_id = getsid(pid);
+    pid_t proc_group_id = getpgrp();
+    printf("session ID: %d,\t process group ID: %d\n", session_id, proc_group_id);
+    // real/effective user id
+    uid_t real_user_id = getuid();
+    uid_t effective_user_id = geteuid();
+    printf("Real user ID: %d,\t effective user ID: %d\n", real_user_id, effective_user_id);
+    // real/effective group id
+    gid_t real_group_id = getgid();
+    gid_t effective_group_id = getegid();
+    printf("Real group ID: %d,\t effective group ID: %d\n", real_group_id, effective_group_id);
+
+    // groups
+    gid_t groups[NGROUPS_MAX + 1];
+    int groups_size = NGROUPS_MAX; 
+    int num_groups = getgroups(groups_size, groups);
+    if (num_groups < 0) {
+        perror("getgroups");
+        return 2;
+    }
+    struct group* gr_info_p;
+    printf("groups=");
+    for (unsigned i = 0; i < (unsigned)num_groups; i++) {
+        printf("%d", groups[i]);
+        gr_info_p = getgrgid(groups[i]);
+        if (gr_info_p == NULL) {
+            printf("-(?)");
+        }
+        else {
+            printf("(%s),", gr_info_p->gr_name);
+        }
+    }
+    putchar('\n');
+
+    #if 0
     // UID
     uid_t real_user_id = getuid(); //These functions are always successful.
     uid_t effecrive_user_id = geteuid();
@@ -102,5 +153,6 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
     printf("Sheduling priority: %d\n", pr);
+    #endif
     return 0;
 }
