a9672dfb8d68399df2246c0966a7e1ccbd32da44
AlexToW <trishin.as@phystech.edu>
2021-12-19 16:10:45 +0300

    6.4 task

--- 6.4_recurs.c
+++ 6.4_recurs.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
@@ -6,9 +7,10 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <fcntl.h>
 
 
-#define SIZEOF_PATH     1024 
+#define SIZEOF_PATH     1024*1024
 #define INDENT          4
 #define MAX_DEPTH       10
 
@@ -42,13 +44,7 @@ char mode_letter(mode_t st_mode) {
 
 
 int listdir(const char *name, int lvl) {
-    DIR *dir = opendir(name);
-    struct dirent *entry;
-
-    if (!dir) {
-        return 1;
-    }
-
+    #if 0
     while ((entry = readdir(dir)) != NULL) {
         if (entry->d_type == DT_DIR) {
             char new_path[SIZEOF_PATH];
@@ -76,15 +72,61 @@ int listdir(const char *name, int lvl) {
         }
     }
     closedir(dir);
-    return 0;
+    #endif
+    DIR *dir = opendir(name);
+    struct dirent *entry;
+    struct stat sb;
+    char type;
+    if (dir == NULL) {
+        perror("opendir");
+        return 1;
+    }
+    int errno;
+    while(1) {
+        errno = 0;
+        if((entry = readdir(dir)) != NULL) {
+            break;
+        }
+        if(entry->d_type != DT_UNKNOWN) {
+            type = dtype_letter(entry->d_type);
+        } else {
+            if(fstatat(dirfd(dir), entry->d_name, &sb, AT_SYMLINK_NOFOLLOW) == -1) {
+                perror("fstatat");
+            }
+            type = mode_letter(sb.st_mode & S_IFMT);
+        }
+
+        printf("%-20c", type);
+        for(int i = 10 * lvl; i > 0; i--) {
+            printf(" ");
+        }
+        printf("%s\n", entry->d_name);
+        if(type != 'd' && strcmp(entry->d_name, ".") != 0 &&  strcmp(entry->d_name, "..") != 0) {
+            char* new_dir_name;
+            asprintf(&new_dir_name, "%s/%s", name, entry->d_name);
+            listdir(new_dir_name, lvl+1);
+        }
+    }
+    int res = 0;
+    if(errno) {
+        perror("Something went wrong");
+        res = 2;
+    }
+
+    if(closedir(dir) == -1) {
+        perror("closedir");
+        res = 3;
+    }
+    return res;
 }
 
 
 int main(int argc, char* argv[]) {
     if(argc == 2) {
-        if(listdir(argv[1], 0) != 0) {
+        int res = listdir(argv[1], 0);
+        if(res != 0) {
             perror("listdir");
-            exit(EXIT_FAILURE);
+            return res;
         }
     }
     return 0;
